b812754258af96f8bca6b6e4ebf0fd48598a6de4
Andrew Codispoti <andrewcod749@gmail.com>
2015-01-23 16:55:05 -0500

    Added bluetooth stuff.

--- app/src/main/java/com/andrew749/speakersync/MainActivity.java
+++ app/src/main/java/com/andrew749/speakersync/MainActivity.java
@@ -1,34 +1,96 @@
 package com.andrew749.speakersync;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.IOException;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     Button connectButton, createButton;
     private BluetoothAdapter bluetoothAdapter;
+    private BluetoothServerSocket serverSocket;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        connectButton=(Button) findViewById(R.id.connectButton);
-        createButton=(Button)findViewById(R.id.createButton);
+        connectButton = (Button) findViewById(R.id.connectButton);
+        createButton = (Button) findViewById(R.id.createButton);
         connectButton.setOnClickListener(this);
         createButton.setOnClickListener(this);
         //static call to get default bluetooth adapter
-        bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
 
+    }
+
+    Runnable acceptConnections = new Runnable() {
+        @Override
+        public void run() {
+            run();
+        }
+    };
+
+
+    private void doClient() {
+
+
+    }
+
+
+    /*Methods for server side of application*/
+    private void doServer() {
+        makeDiscoverable();
         //intent to turn on bluetooth
         Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
         startActivityForResult(turnOn, 0);
+        while (serverSocket == null) ;
+        AcceptThread();
+
+    }
+
+    private void makeDiscoverable() {
+        Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivity(discoverableIntent);
+    }
+
+    public void AcceptThread() {
+        BluetoothServerSocket tmp = null;
+        try {
+            tmp = bluetoothAdapter.listenUsingRfcommWithServiceRecord("MYYAPP", MY_UUID_SECURE);
+
+        } catch (IOException e) {
+        }
+        serverSocket = tmp;
+    }
+
+    public void run() {
+        BluetoothSocket socket = null;
+        while (true) {
+            try {
+                socket = serverSocket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (socket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
     }
 
 
@@ -53,16 +115,16 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         return super.onOptionsItemSelected(item);
     }
+
     @Override
     public void onClick(View v) {
-        int id=v.getId();
-        if(id==R.id.connectButton){
+        int id = v.getId();
+        if (id == R.id.connectButton) {
             //do connect action
 
-        }
-        else if (id==R.id.createButton){
+        } else if (id == R.id.createButton) {
             //do create action
-
+            doServer();
 
         }
     }
