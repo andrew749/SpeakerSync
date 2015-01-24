1697732b6ac3e32d351f718ca840fe8d157c9847
Andrew Codispoti <andrewcod749@gmail.com>
2015-01-24 17:29:36 -0500

    Code to play music from byte array.

--- app/src/main/java/com/andrew749/speakersync/MainActivity.java
+++ app/src/main/java/com/andrew749/speakersync/MainActivity.java
@@ -1,22 +1,28 @@
 package com.andrew749.speakersync;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothServerSocket;
-import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
     Button connectButton, createButton;
-
+    WifiP2pManager mManager;
+    WifiP2pManager.Channel mChannel;
+    BroadcastReceiver mReceiver;
+    IntentFilter mIntentFilter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +32,15 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         createButton = (Button) findViewById(R.id.createButton);
         connectButton.setOnClickListener(this);
         createButton.setOnClickListener(this);
-        //static call to get default bluetooth adapter
 
+        mManager = (WifiP2pManager) getSystemService(Context.WIFI_P2P_SERVICE);
+        mChannel = mManager.initialize(this, Looper.getMainLooper(), null);
+        mReceiver = new SongBroadcastReceiver();
+        mIntentFilter = new IntentFilter();
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
+        mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
     }
 
@@ -39,11 +52,33 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     };
 
 
-    private void doClient() {
 
+    private void serachForPeers() {
+        mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+
+            }
+
+            @Override
+            public void onFailure(int reasonCode) {
+
+            }
+        });
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(mReceiver, mIntentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mReceiver);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -77,6 +112,55 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             //do create action
 
 
+        }
+    }
+
+    private byte[] convertToByteStream() {
+        FileInputStream fileInputStream = null;
+
+        File file = new File("C:\\testing.txt");
+
+        byte[] bFile = new byte[(int) file.length()];
+
+        try
+
+        {
+            //convert file into array of bytes
+            fileInputStream = new FileInputStream(file);
+            fileInputStream.read(bFile);
+            fileInputStream.close();
+
+        } catch (
+                Exception e
+                )
+
+        {
+            e.printStackTrace();
+        }
+        return bFile;
+    }
+
+    class SongBroadcastReceiver extends BroadcastReceiver {
+        private WifiP2pManager mManager;
+        WifiP2pManager.Channel channel;
+
+        public SongBroadcastReceiver() {
+        }
+
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+                // Check to see if Wi-Fi is enabled and notify appropriate activity
+            } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+                // Call WifiP2pManager.requestPeers() to get a list of current peers
+            } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+                // Respond to new connection or disconnections
+            } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+                // Respond to this device's wifi state changing
+            }
         }
     }
 
--- app/src/main/java/com/andrew749/speakersync/Player.java
+++ app/src/main/java/com/andrew749/speakersync/Player.java
@@ -1,7 +1,13 @@
 package com.andrew749.speakersync;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by andrew on 1/24/15.
@@ -12,5 +18,32 @@ public class Player extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    private void playMp3(byte[] mp3SoundByteArray) {
+        try {
+            // create temp file that will hold byte array
+            File tempMp3 = File.createTempFile("kurchina", "mp3", getCacheDir());
+            tempMp3.deleteOnExit();
+            FileOutputStream fos = new FileOutputStream(tempMp3);
+            fos.write(mp3SoundByteArray);
+            fos.close();
+
+            // Tried reusing instance of media player
+            // but that resulted in system crashes...
+            MediaPlayer mediaPlayer = new MediaPlayer();
+
+            // Tried passing path directly, but kept getting
+            // "Prepare failed.: status=0x1"
+            // so using file descriptor instead
+            FileInputStream fis = new FileInputStream(tempMp3);
+            mediaPlayer.setDataSource(fis.getFD());
+
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (IOException ex) {
+            String s = ex.toString();
+            ex.printStackTrace();
+        }
     }
 }
