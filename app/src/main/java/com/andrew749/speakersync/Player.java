2ee937c76f8b54d2f6da3d9826070c8454daa1df
Andrew Codispoti <andrewcod749@gmail.com>
2015-01-24 17:06:31 -0500

    Added song class and broadcastreceiver class.

---
+++ app/src/main/java/com/andrew749/speakersync/Player.java
@@ -0,0 +1,16 @@
+package com.andrew749.speakersync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by andrew on 1/24/15.
+ */
+public class Player extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
